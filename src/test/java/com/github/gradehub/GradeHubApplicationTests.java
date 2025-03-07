d873255cf190c7ece610948866d08e5a0d39638e
Jhon <jhontenorio1618@gmail.com>
2025-03-06 21:59:32 -0600

    Environment variables bug: DEBBUGED

--- src/test/java/com/github/gradehub/GradeHubApplicationTests.java
+++ src/test/java/com/github/gradehub/GradeHubApplicationTests.java
@@ -1,5 +1,7 @@
 package com.github.gradehub;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,13 @@ class GradeHubApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @BeforeAll
+    static void setup() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("TEST_DB_PASSWORD", dotenv.get("TEST_DB_PASSWORD"));
+        System.setProperty("PROD_DB_PASSWORD", dotenv.get("PROD_DB_PASSWORD"));
     }
 
 }
